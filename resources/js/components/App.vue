a3bf7451195691d7594edab301b663339aa9f367
Mattia <mcortinovi9@gmail.com>
2022-01-17 17:40:16 +0100

    Esercizio completato

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ helloMsg }}</h1>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            helloMsg: "Benvenuto in",
+        };
+    },
+};
+</script>
+
+<style></style>
